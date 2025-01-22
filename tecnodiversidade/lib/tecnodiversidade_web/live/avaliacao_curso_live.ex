9f49bb158b36aa546b1c2bbd1fcdc05f9850392e
FabriDamazio <fabridamazio@gmail.com>
2025-01-22 16:00:28 -0300

    Conteudo tela avaliação do curso

--- tecnodiversidade/lib/tecnodiversidade_web/live/avaliacao_curso_live.ex
+++ tecnodiversidade/lib/tecnodiversidade_web/live/avaliacao_curso_live.ex
@@ -10,23 +10,27 @@ defmodule TecnodiversidadeWeb.AvaliacaoCursoLive do
     <section class="bg-gray-900 text-white">
       <div class="container mx-auto px-4 py-16 flex flex-col items-center text-center space-y-6 sm:space-y-8 lg:space-y-10">
         <h1 class="text-4xl sm:text-5xl lg:text-6xl font-bold tracking-tight">
-          Avaliação do Curso
+          Participe da pesquisa e avalie o curso
         </h1>
       </div>
     </section>
 
     <section class="body-text py-8 px-12 max-w-7xl mx-auto">
       <p class="mt-4">
-        Concluímos o módulo 1 nos aprofundando nos estudos de dois pensadores fundamentais no campo da filosofia da tecnologia: Álvaro Vieira Pinto e Andrew Feenberg. Suas contribuições nos convidam a desconstruir visões simplistas e deterministas sobre a tecnologia, revelando-a como um campo complexo, atravessado por relações de poder, valores sociais e possibilidades de transformação.
+        Considerando a importância de alinhar nosso trabalho aos objetivos do Programa de Mestrado Profissional em Educação Profissional e Tecnológica, desenvolvemos este curso baseado na pesquisa “Intersecções entre Tecnologia e Educação Profissional e Tecnológica: Desafios e Perspectivas para a Tecnodiversidade”, conduzida pela mestranda Luciana Garcia dos Reis, sob a orientação da Profa. Dra. Luciana dos Santos Rosenau.
       </p>
       <p class="mt-4">
-        Vieira Pinto nos apresentou uma perspectiva crítica compreendendo a tecnologia não como um elemento externo ou neutro, mas como uma produção humana intrinsecamente conectada aos projetos de sociedade. Já Feenberg aprofundou essa discussão através do construtivismo crítico, demonstrando como cada artefato tecnológico carrega em si narrativas políticas, sociais e culturais.
+        Para finalizar esta etapa, contamos mais uma vez com o seu apoio na avaliação do curso. Sua participação é muito importante.
       </p>
       <p class="mt-4">
-        No próximo módulo, exploraremos o conceito de tecnodiversidade desenvolvido pelo filósofo Yuk Hui. Suas reflexões nos convidarão a compreender a tecnologia para além de uma lógica ocidental e universal, reconhecendo as múltiplas cosmologias e formas de relação com os sistemas técnicos.
+        O tempo estimado para responder é de aproximadamente 5 (cinco) minutos.
       </p>
-      <p class="mt-4">
-        Até o próximo módulo!
+      <p class="mt-4 mb-8 ">
+        Acesse o formulário de avaliação: <a
+          href="https://docs.google.com/forms/d/e/1FAIpQLScYRsUSgMtGlixfrSMGn0PiMhSywVSbxaGFYiDQ7W_1p4WOVQ/viewform"
+          target="_blank"
+          class="text-blue-500 hover:underline"
+        >https://docs.google.com/forms/d/e/1FAIpQLScYRsUSgMtGlixfrSMGn0PiMhSywVSbxaGFYiDQ7W_1p4WOVQ/viewform</a>.
       </p>
     </section>
     """
