f529125feefea0cc44975d1d88a63876b361766d
FabriDamazio <fabridamazio@gmail.com>
2025-01-25 12:53:50 -0300

    Desabilita envio de email de confirmação até ter servidor de email configurado

--- tecnodiversidade/lib/tecnodiversidade_web/live/user_registration_live.ex
+++ tecnodiversidade/lib/tecnodiversidade_web/live/user_registration_live.ex
@@ -55,11 +55,12 @@ defmodule TecnodiversidadeWeb.UserRegistrationLive do
   def handle_event("save", %{"user" => user_params}, socket) do
     case Accounts.register_user(user_params) do
       {:ok, user} ->
-        {:ok, _} =
-          Accounts.deliver_user_confirmation_instructions(
-            user,
-            &url(~p"/users/confirm/#{&1}")
-          )
+        # utilizar quando tiver o servidor de e-mail configurado
+        # {:ok, _} =
+        #    Accounts.deliver_user_confirmation_instructions(
+        #    user,
+        #   &url(~p"/users/confirm/#{&1}")
+        #  )
 
         changeset = Accounts.change_user_registration(user)
         {:noreply, socket |> assign(trigger_submit: true) |> assign_form(changeset)}
